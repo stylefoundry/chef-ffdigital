470efb6be5f3a234ccde4a815533adfd9b309c4c
Justin Boynton <justin.boynton@stylefoundry.co.uk>
2016-04-08 08:29:33 +0100

    updating version number

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'justin@spicerack.co.uk'
 license          'All rights reserved'
 description      'Installs/Configures ffdigital'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '0.5.0'
